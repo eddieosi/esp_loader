92e2a8a7388234a6ef341d5f902b794e443ae913
Dennis Mandel <d.mandel@bitergo.de>
2022-03-31 00:27:31 +0200

    fixed sync issue

--- footable-load.js
+++ footable-load.js
@@ -1,66 +1,68 @@
 $(document).ready(function () {
-    
-    console.log("loaded footable-load.js");
-    var $modal = $('#editor-modal'),
-        $editor = $('#editor'),
-        $editorTitle = $('#editor-title'),
-        ft = FooTable.init('#esp-data', {
-            columns: $.get("https://raw.githubusercontent.com/eddieosi/esp_loader/main/columns.json"),
-            rows: jsonData,
-            editing: {
-                addRow: function () {
-                    $modal.removeData('row');
-                    $editor[0].reset();
-                    $editorTitle.text('Add a new row');
-                    $modal.modal('show');
-                },
-                editRow: function (row) {
-                    var values = row.val();
-                    $editor.find('#firstName').val(values.firstName);
-                    $editor.find('#lastName').val(values.lastName);
-                    $editor.find('#message').val(values.message);
-                    $modal.data('row', row);
-                    $editorTitle.text('Edit row #' + values.firstName);
-                    $modal.modal('show');
-                },
-                deleteRow: function (row) {
-                    if (confirm('Are you sure you want to delete the row?')) {
-                        row.delete();
-                        var contains = containsObject(row.val().firstName, jsonData);
-                        if(contains !== false){
-                            jsonData.splice(contains, 1);
+    $.get("https://raw.githubusercontent.com/eddieosi/esp_loader/main/columns.json", function(columnData){
+        console.log("loaded footable-load.js");
+        var $modal = $('#editor-modal'),
+            $editor = $('#editor'),
+            $editorTitle = $('#editor-title'),
+            ft = FooTable.init('#esp-data', {
+                columns: columnData,
+                rows: jsonData,
+                editing: {
+                    addRow: function () {
+                        $modal.removeData('row');
+                        $editor[0].reset();
+                        $editorTitle.text('Add a new row');
+                        $modal.modal('show');
+                    },
+                    editRow: function (row) {
+                        var values = row.val();
+                        $editor.find('#firstName').val(values.firstName);
+                        $editor.find('#lastName').val(values.lastName);
+                        $editor.find('#message').val(values.message);
+                        $modal.data('row', row);
+                        $editorTitle.text('Edit row #' + values.firstName);
+                        $modal.modal('show');
+                    },
+                    deleteRow: function (row) {
+                        if (confirm('Are you sure you want to delete the row?')) {
+                            row.delete();
+                            var contains = containsObject(row.val().firstName, jsonData);
+                            if(contains !== false){
+                                jsonData.splice(contains, 1);
+                            }
                         }
                     }
                 }
+            }),
+            uid = 10001;
+    
+        $editor.on('submit', function (e) {
+            if (this.checkValidity && !this.checkValidity()) return;
+            e.preventDefault();
+            var row = $modal.data('row'),
+                values = {
+                    firstName: $editor.find('#firstName').val(),
+                    lastName: $editor.find('#lastName').val(),
+                    message: $editor.find('#message').val(),
+                };
+    
+                var contains = containsObject(row.val().firstName, jsonData);
+            if(contains === false){
+                jsonData.push(values);
+            }else{
+                jsonData[contains] = values;
             }
-        }),
-        uid = 10001;
-
-    $editor.on('submit', function (e) {
-        if (this.checkValidity && !this.checkValidity()) return;
-        e.preventDefault();
-        var row = $modal.data('row'),
-            values = {
-                firstName: $editor.find('#firstName').val(),
-                lastName: $editor.find('#lastName').val(),
-                message: $editor.find('#message').val(),
-            };
-
-            var contains = containsObject(row.val().firstName, jsonData);
-        if(contains === false){
-            jsonData.push(values);
-        }else{
-            jsonData[contains] = values;
-        }
-        if (row instanceof FooTable.Row) {
-            row.val(values);
-        } else {
-            ft.rows.add(values);
-        }
-        
-        
-        $modal.modal('hide');
-    });
+            if (row instanceof FooTable.Row) {
+                row.val(values);
+            } else {
+                ft.rows.add(values);
+            }
+            
+            
+            $modal.modal('hide');
+        });
+    })
+    
 });
 
 function containsObject(obj, list) {
